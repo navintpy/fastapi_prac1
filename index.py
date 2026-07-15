c18b608b7dc403e993923d1afd40a973adeb5d62
navintatikonda93 <nnt3111@gmail.com>
2026-07-15 22:10:44 +0530

    Initial commit #1

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from routes.index import user
+
 app = FastAPI()
 
-@app.get("/")
-def read_something():
-    return {"msg":"Hello World"}
+app.include_router(user)
+
+# Below code comment using route from routes folder and above include
+# @app.get("/")
+# def read_something():
+#     return {"msg":"Hello World"}
