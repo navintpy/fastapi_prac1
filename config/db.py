53246ee3fce37374a35fa3fc22249cca318aa437
navintatikonda93 <nnt3111@gmail.com>
2026-07-22 07:58:42 +0530

    database connection change now dotenv package using

--- config/db.py
+++ config/db.py
@@ -1,6 +1,24 @@
 from sqlalchemy import create_engine, MetaData
+import os
+from dotenv import load_dotenv
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/pytest")
+# from urllib.parse import quote_plus
+
+load_dotenv()
+
+# password = quote_plus(os.getenv("DB_PASSWORD"))
+# f"{os.getenv('DB_USER')}:{password}@"  # USE This only line of code below in Password section
+
+DATABASE_URL = (
+    f"mysql+pymysql://"
+    f"{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/"
+    f"{os.getenv('DB_NAME')}"
+)
+
+engine = create_engine(DATABASE_URL)
 
 meta = MetaData()
 
